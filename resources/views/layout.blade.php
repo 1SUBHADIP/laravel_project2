9e5f8cc997ecde8d1dbcaa3cbb327153dfcc1d00
1SUBHADIP <nayaksubhadip88@gmail.com>
2026-04-26 13:52:15 +0530

    fix google verification

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,9 +2,9 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+  
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="google-site-verification" content="1vt7G1ESZjsGEIHDy7jJbuyBrWh6NqoLuuzuMKsSDuQ" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'Library Management System in Laravel')</title>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,6 +7,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <meta name="google-site-verification" content="1vt7G1ESZjsGEIHDy7jJbuyBrWh6NqoLuuzuMKsSDuQ" />
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>CCLMS Library | Smart Circulation Hub</title>
