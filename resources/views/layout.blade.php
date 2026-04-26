733adc27ec876fb1df87e7fbd701082e252fbdd0
1SUBHADIP <nayaksubhadip88@gmail.com>
2026-04-26 14:58:46 +0530

    fix google site verification

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  
+  <meta name="google-site-verification" content="1vt7G1ESZjsGEIHDy7jJbuyBrWh6NqoLuuzuMKsSDuQ" />
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
